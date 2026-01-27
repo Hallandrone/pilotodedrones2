8c16356136038c9b8d75e1388938ede9cfbe4e41
Thealvarro <cofre@live.cl>
2026-01-27 20:11:04 -0300

    2

--- src/components/DiplomaPDF.tsx
+++ src/components/DiplomaPDF.tsx
@@ -164,7 +164,7 @@ const styles = StyleSheet.create({
 	},
 	qrCode: {
 		position: 'absolute',
-		left: 695,
+		left: 702,
 		top: 72,
 		width: 61,
 		height: 61,
@@ -242,7 +242,7 @@ const styles = StyleSheet.create({
 	validationCode: {
 		position: 'absolute',
 		top: 135,
-		left: 666,
+		left: 673,
 		width: 120,
 		fontSize: 5.5,
 		fontFamily: 'Helvetica',
@@ -252,7 +252,7 @@ const styles = StyleSheet.create({
 	legalText: {
 		position: 'absolute',
 		top: 155,
-		left: 666,
+		left: 673,
 		width: 120,
 		fontSize: 4.5,
 		fontFamily: 'Helvetica',
